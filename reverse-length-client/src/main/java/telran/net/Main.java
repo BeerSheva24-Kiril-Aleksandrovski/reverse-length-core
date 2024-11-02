b85cdf6714e6dfa266cc3d394225f949014dce6c
Kiril <fanky.yahoo@gmail.com>
2024-11-02 17:19:06 +0200

    Project created, HW #37 done according to README

---
+++ reverse-length-client/src/main/java/telran/net/Main.java
@@ -0,0 +1,43 @@
+package telran.net;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import telran.view.InputOutput;
+import telran.view.Item;
+import telran.view.Menu;
+import telran.view.StandardInputOutput;
+
+public class Main {
+    final static HashSet<String> operationsSet = new HashSet<String>(Arrays.asList("reverse", "length"));
+    static EchoClient echoClient;
+
+    public static void main(String[] args) {
+
+        Item[] items = {
+                Item.of("Get started!", Main::startSession),
+                Item.ofExit()
+        };
+        Menu menu = new Menu("Take a Length or Reverse the String Application", items);
+        menu.perform(new StandardInputOutput());
+    }
+
+    static void startSession(InputOutput io) {
+        String host = io.readString("Enter hostname");
+        int port = io.readNumberRange("Enter port", "Wrong port", 3000, 50000).intValue();
+        if (echoClient != null) {
+            echoClient.close();
+        }
+        echoClient = new EchoClient(host, port);
+        String string = io.readString("Enter any string");
+        String operationType = io.readStringOptions("Chose operation from list:" + operationsSet.toString(), "Wrong operation", operationsSet);
+        String response = echoClient.sendAndReceive(string, operationType);
+    
+        io.writeLine("\nConnected to " + host + ":" + port);
+        io.writeLine("Sent: " + string);
+        io.writeLine("Operation:" + operationType);
+        io.writeLine("Received response: " + response);
+        
+
+    }
+}
---
+++ reverse-length-server/src/main/java/telran/net/Main.java
@@ -0,0 +1,45 @@
+package telran.net;
+
+import java.net.*;
+import java.io.*;
+
+public class Main {
+    private static final int PORT = 4000;
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            runSession(socket);
+        }
+    }
+
+    private static void runSession(Socket socket) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintStream writer = new PrintStream(socket.getOutputStream())) {
+            String operationType;
+            while ((operationType = reader.readLine()) != null) {
+                String receivedString = reader.readLine();
+                String responseString;
+
+                switch (operationType) {
+                    case "length":
+                        responseString = String.valueOf(receivedString.length());
+                        break;
+                    case "reverse":
+                        responseString = new StringBuilder(receivedString).reverse().toString();
+                        break;
+                    default:
+                        responseString = "No such operation";
+                        break;
+                }
+                
+                writer.println(responseString);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Client closed connection abnormally");
+        }
+    }
+}
